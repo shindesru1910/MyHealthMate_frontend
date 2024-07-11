7292181ddc1a6248d3f1dcc9013c6a2d400e23bb
shindesru1910 <119733618+shindesru1910@users.noreply.github.com>
2024-07-11 14:52:13 +0530

    Made a UserTable Compont to get the user data

--- myhealthmate_frontend/src/App.js
+++ myhealthmate_frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import UserTable from './components/UserTable';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          MyHealthMate
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<UserTable />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ myhealthmate_frontend/src/components/UserTable.jsx
@@ -0,0 +1,56 @@
+// src/components/UserTable.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function UserTable() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get('http://127.0.0.1:8000/get-user')
+      .then(response => {
+        if (response.data.status === 200) {
+          setUsers(response.data.data);
+        } else {
+          console.error("Error fetching data");
+        }
+      })
+      .catch(error => {
+        console.error("There was an error fetching the users!", error);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <div className="container mt-4">
+      <h2 className="text-center mb-4">User List</h2>
+      {loading ? (
+        <div className="text-center">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <table class="table table-dark table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Phone</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.phone}</td>
+                <td>{user.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}

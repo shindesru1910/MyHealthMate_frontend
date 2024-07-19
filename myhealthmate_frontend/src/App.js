28df4f1105a743c0795657e2e180e7e5aa5d3fd8
Shagufta Saiyed <shaguftasaiyed25@gmail.com>
2024-07-19 14:21:46 +0530

    javascript css

--- myhealthmate_frontend/src/App.js
+++ myhealthmate_frontend/src/App.js
@@ -3,23 +3,13 @@
 // src/App.js
 import React , { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-// import Header from './components/header';
-// import HeroSection from './components/herosection';
-// import AboutSection from './components/aboutsection';
-// import StatsSection from './components/statssection';
-// import ServicesSection from './components/servicessection';
-// import AppointmentSection from './components/appointmentsection';
-// import DoctorsSection from './components/doctorsection';
-// import TestimonialSection from './components/testimonialsection';
-// import GallerySection from './components/gallerysection';
-// import ContactSection from './components/contactsection';
-// import Footer from './components/footer';
+// import Home from './components/Home'
 import Home from './components/Homepage';
 import UserTable from './components/UserTable';
 import AdminPage from './components/AdminPage';
 import UserPage from './components/UserPage';
-// import UserLogin from './components/UserLogin';
+import UserLogin from './components/UserLogin';
+import Homepage from './components/Homepage';
 
 function App() {
 
@@ -38,11 +28,11 @@ function App() {
         <ContactSection />
         <Footer /> */}
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Homepage />} />
           <Route path="/userlist" element={<UserTable />} />
-          <Route path="/adminpage" element={<AdminPage />} />
-          <Route path="/Userpage" element={<UserPage />} />
-          {/* <Route path="/userlogin" element={<UserLogin />} /> */}
+          <Route path="/adminpage" element={<AdminPage/>} />
+          <Route path="/Userpage" element={<UserPage/>} />
+          <Route path="/userlogin" element={<UserLogin/>} />
         </Routes>
       </div>
     </Router>
@@ -53,35 +43,3 @@ export default App;
 
 
 
-// // src/App.js
-// import React from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap-icons/font/bootstrap-icons.css';
-// import PureCounter from '@srexi/purecounterjs';
-
-
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// // import Home from './components/Home'
-// import Home from './components/Home';
-// import UserTable from './components/UserTable';
-// import AdminPage from './components/AdminPage';
-// import UserPage from './components/UserPage';
-// import UserLogin from './components/UserLogin';
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/userlist" element={<UserTable />} />
-//           <Route path="/adminpage" element={<AdminPage/>} />
-//           <Route path="/Userpage" element={<UserPage/>} />
-//           <Route path="/userlogin" element={<UserLogin/>} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;

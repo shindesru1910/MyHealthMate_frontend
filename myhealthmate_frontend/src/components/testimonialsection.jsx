f0e7caa83b388369babf84d7257d035dacbe6ce6
Shagufta Saiyed <shaguftasaiyed25@gmail.com>
2024-07-22 14:09:01 +0530

    testimonial section done

--- myhealthmate_frontend/src/components/testimonialsection.jsx
+++ myhealthmate_frontend/src/components/testimonialsection.jsx
@@ -1,79 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './testimonial.css'; 
 
 const TestimonialsSection = () => {
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/get-feedback')
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 200) {
+          setTestimonials(data.data);
+        } else {
+          console.error('Error fetching testimonials:', data.msg);
+        }
+      })
+      .catch(error => console.error('Error fetching testimonials:', error));
+  }, []);
+
+  const nextTestimonial = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonials.length);
+  };
+
+  const prevTestimonial = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + testimonials.length) % testimonials.length);
+  };
+
+  if (testimonials.length === 0) {
+    return <p>Loading testimonials...</p>;
+  }
+
+  const { user, feedback_text: text } = testimonials[currentIndex];
+  const userName = user ? `${user.first_name} ${user.last_name}` : 'Anonymous';
+
   return (
     <section id="testimonials" className="testimonials section">
       <div className="container">
         <div className="row align-items-center">
-          <div className="col-lg-5 info" data-aos-delay="100">
+          <div className="col-lg-5 info">
             <h3>Testimonials</h3>
             <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
+              Hear from our satisfied users about their experiences with our service. We value your feedback and strive to improve continuously.
             </p>
           </div>
 
-          <div className="col-lg-7"  data-aos-delay="200">
-            <div className="swiper init-swiper">
-              <div className="swiper-wrapper">
-                {/* Testimonial Items */}
-
-                <div className="swiper-slide">
-                  <div className="testimonial-item">
-                    <div className="d-flex">
-                      <img src="assets/img/testimonials/testimonials-1.jpg" className="testimonial-img flex-shrink-0" alt="" />
-                      <div>
-                        <h3>Saul Goodman</h3>
-                        <h4>Ceo & Founder</h4>
-                        <div className="stars">
-                          <i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i>
-                        </div>
-                      </div>
+          <div className="col-lg-7">
+            <div className="slider">
+              <button className="slider-button prev" onClick={prevTestimonial}>❮</button>
+              <div className="slider-content">
+                <div className="testimonial-item">
+                  <div className="d-flex">
+                    <div>
+                      <h3>{userName}</h3>
+                      <h4>Feedback</h4>
                     </div>
-                    <p>
-                      <i className="bi bi-quote quote-icon-left"></i>
-                      <span>Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.</span>
-                      <i className="bi bi-quote quote-icon-right"></i>
-                    </p>
                   </div>
+                  <p>
+                    <i className="bi bi-quote quote-icon-left"></i>
+                    <span>{text}</span>
+                    <i className="bi bi-quote quote-icon-right"></i>
+                  </p>
                 </div>
-               
-
-                <div className="swiper-slide">
-                  <div className="testimonial-item">
-                    <div className="d-flex">
-                      <img src="assets/img/testimonials/testimonials-1.jpg" className="testimonial-img flex-shrink-0" alt="" />
-                      <div>
-                        <h3>Sabadman</h3>
-                        <h4>Founder</h4>
-                        <div className="stars">
-                          <i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i>
-                        </div>
-                      </div>
-                    </div>
-                    <p>
-                      <i className="bi bi-quote quote-icon-left"></i>
-                      <span>Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.</span>
-                      <i className="bi bi-quote quote-icon-right"></i>
-                    </p>
-                  </div>
-                </div>
-               
-                {/* Repeat for other testimonials */}
-                {/* You can add more swiper-slide divs for each testimonial */}
-
               </div>
-              <div className="swiper-pagination"></div>
+              <button className="slider-button next" onClick={nextTestimonial}>❯</button>
             </div>
           </div>
         </div>
       </div>
-      
-      
     </section>
   );
 };
 
 export default TestimonialsSection;
-
-

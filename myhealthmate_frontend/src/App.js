6f6692dc0178575b652208802b9817e970776879
shindesru1910 <119733618+shindesru1910@users.noreply.github.com>
2024-08-06 11:48:13 +0530

    Fixed chatbot bug

--- myhealthmate_frontend/src/App.js
+++ myhealthmate_frontend/src/App.js
@@ -1,8 +1,5 @@
-// new
-
-import React , { useEffect } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Home from './components/Home'
 import UserTable from './components/UserTable';
 import AdminPage from './components/AdminPage';
 import UserPage from './components/UserPage';
@@ -29,55 +26,42 @@ import AdminRoute from './common/AdminRoute';
 import Exercise from './components/Exercise';
 import Diet from './components/Diet';
 
-
 axios.defaults.baseURL = 'http://localhost:8000';
 
 function App() {
+  const [chatbotOpen, setChatbotOpen] = useState(false);
+
+  const toggleChatbot = () => {
+    setChatbotOpen(prevState => !prevState);
+  };
 
   return (
     <Router>
       <div className="App">
-       
-
         {chatbotOpen && <ChatbotComponent />}
         <ChatbotToggleButton onClick={toggleChatbot} />
         
         <Routes>
-        {/* <Route path="/" element={<ChangeRoute/>} /> */}
           <Route path="/" element={<Homepage />} />
           <Route path="/userlist" element={<UserTable />} />
           <Route path="/adminpage" element={<AdminPage />} />
           <Route path="/Userpage" element={<UserPage />} />
           <Route path="/userlogin" element={<UserLogin />} />
-          <Route path="/doctorlist" element={<DoctorTable />} />
+          <Route path="/doctorlist" element={<AdminRoute><DoctorTable/></AdminRoute>} />
           <Route path="/register" element={<RegistrationForm />} />
           <Route path="/userdata" element={<UserData />} />
-          <Route path="/user-management" element={<UserManagement />} />
-          <Route path="/doctor-management" element={<DoctorManagement />} />
-          <Route path="/add-doctor" element={<AddDoctor />} />
-          <Route path="/doctor" element={<Doctor />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/premiumpage" element={<PremiumPage />} />
-          <Route path="/adminpage" element={<AdminPage/>} />
-          <Route path="/Userpage" element={<UserPage/>} />
-          <Route path="/userlogin" element={<UserLogin/>} />
-          <Route path="/doctorlist" element={<AdminRoute><DoctorTable/></AdminRoute>} />
-          <Route path="/register" element={<RegistrationForm/>} />
-          <Route path="/userdata" element={<UserData/>} />
-          <Route path="/user-management" element={<UserManagement/>} />
-          <Route path="/doctor-management" element={<DoctorManagement/>} />
           <Route path="/user-management" element={<AdminRoute><UserManagement/></AdminRoute>} />
           <Route path="/doctor-management" element={<AdminRoute><DoctorManagement/></AdminRoute>} />
-          <Route path="/add-doctor" element={<AddDoctor/>} />
-          <Route path="/doctor" element={<Doctor/>} />
-          <Route path="/users" element={<User/>} />
-          <Route path="/auth" element={<Auth/>} />
-          <Route path="/premiumpage" element={<PremiumPage/>} />
-          <Route path="/password-reset" element={<ForgotPassword/>} />
-          <Route path="/get-feedback" element={<GetFeedback/>} />
-          <Route path="/appointment-form" element={<AppointmentSection/>} />
-          <Route path="/exercise" element={<Exercise/>} />
-          <Route path="/diet" element={<Diet/>} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor" element={<Doctor />} />
+          <Route path="/users" element={<User />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/premiumpage" element={<PremiumPage />} />
+          <Route path="/password-reset" element={<ForgotPassword />} />
+          <Route path="/get-feedback" element={<GetFeedback />} />
+          <Route path="/appointment-form" element={<AppointmentSection />} />
+          <Route path="/exercise" element={<Exercise />} />
+          <Route path="/diet" element={<Diet />} />
         </Routes>
       </div>
     </Router>
@@ -85,6 +69,3 @@ function App() {
 }
 
 export default App;
-
-
-
